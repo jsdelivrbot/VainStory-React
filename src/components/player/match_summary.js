583f7b1fc825acebcfe7859d66438bf62bc47395
HyunsooPark <qkrgustnrk@gmail.com>
2018-03-07 17:18:21 +0900

    match summary base

--- src/components/player/match_summary.js
+++ src/components/player/match_summary.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
+import {List, ListItem} from 'material-ui/List';
+import Avatar from 'material-ui/Avatar';
+import {grey400, darkBlack, lightBlack} from 'material-ui/styles/colors';
 
 class MatchSummary extends Component {
   constructor(props) {
@@ -33,23 +36,120 @@ class MatchSummary extends Component {
       background: '#121212'
     }
 
+    const backStyle = {
+      position: 'absolute',
+      zIndex: '1'
+    }
+
+    const frontStyle = {
+      zIndex: '2'
+    }
+
     return (
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-5">
-            <CircularProgress
-              
-              mode="determinate"
-              value={this.state.completed}
-              size={60}
-              thickness={7}
-            />
+              <div className="col-xs-6">
+                <p>20전 7승 12패</p>
+                <CircularProgress
+                  color="red"
+                  style={backStyle}
+                  mode="determinate"
+                  value={100}
+                  size={120}
+                  thickness={7}
+                />
+                <CircularProgress
+                  style={frontStyle}
+                  mode="determinate"
+                  value={this.state.completed}
+                  size={120}
+                  thickness={7}
+                />
+              </div>
+              <div className="col-xs-6">
+                <p>7.7 / 5.1 / 6.4</p>
+                <h4>2.76:1 (56%)</h4>
+              </div>
           </div>
           <div className="col-xs-4">
-          
+            <List>
+              <ListItem
+                leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                primaryText="Brunch this weekend?"
+                secondaryText={
+                  <p>
+                    <span style={{color: darkBlack}}>Brendan Lim</span> --
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2}
+              />
+              <ListItem
+                leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                primaryText="Brunch this weekend?"
+                secondaryText={
+                  <p>
+                    <span style={{color: darkBlack}}>Brendan Lim</span> --
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2}
+              />
+              <ListItem
+                leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                primaryText="Brunch this weekend?"
+                secondaryText={
+                  <p>
+                    <span style={{color: darkBlack}}>Brendan Lim</span> --
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2}
+              />
+            </List>
           </div>
           <div className="col-xs-3">
-          
+            <p>선호 포지션</p>
+            <List>
+              <ListItem
+                leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                primaryText="Brunch this weekend?"
+                secondaryText={
+                  <p>
+                    <span style={{color: darkBlack}}>Brendan Lim</span> --
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2}
+              />
+            </List>
+            <List>
+              <ListItem
+                leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                primaryText="Brunch this weekend?"
+                secondaryText={
+                  <p>
+                    <span style={{color: darkBlack}}>Brendan Lim</span> --
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2}
+              />
+            </List>
+            <List>
+              <ListItem
+                leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                primaryText="Brunch this weekend?"
+                secondaryText={
+                  <p>
+                    <span style={{color: darkBlack}}>Brendan Lim</span> --
+                    I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                  </p>
+                }
+                secondaryTextLines={2}
+              />
+            </List>
           </div>
         </div>
       </div>
